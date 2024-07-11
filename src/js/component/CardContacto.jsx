d4e70408e3c3df531f63f66933026d0be9d9990a
Loisinho <159461422+loisinho87@users.noreply.github.com>
2024-07-11 10:11:00 +0000

    listo, falta poner iconos

--- src/js/component/CardContacto.jsx
+++ src/js/component/CardContacto.jsx
@@ -1,15 +1,18 @@
-/*import React from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { useContext } from "react";
 
-const CardContactos = ({ contacto, borrarContacto }) => { // recibe props desestructurados. Aquí contacto representa los datos de cada contacto y borrarContacto
+const CardContactos = ({name, phone, email, address, id }) => { // recibe props desestructurados. Aquí contacto representa los datos de cada contacto y borrarContacto
+  const{store, actions} = useContext(Context)
   return (
     <div className="card mb-3">
       <div className="card-body">
-        <h5 className="card-title">{contacto.name}</h5> //Muestro el nombre del contacto con la clase 
-        <h6 className="card-subtitle mb-2 text-muted">{contacto.phone}</h6> // Muestro el teléfono del contacto
-        <p className="card-text">{contacto.email}</p> //Muestro el teléfono el email
-        <p className="card-text">{contacto.address}</p> // Muestro la dirección
-        <button className="btn btn-danger" onClick={() => borrarContacto(contacto.id)}>
+       <h5 className="card-title">{name}</h5>   {/*Muestro el nombre del contacto con la clase */}
+        <h6 className="card-subtitle mb-2 text-muted">{phone}</h6> {/*Muestro el teléfono del contacto*/}
+        <p className="card-text">{email}</p> {/*Muestro el teléfono el email */}
+        <p className="card-text">{address}</p> {/*Muestro la dirección */}
+        <button className="btn btn-danger" onClick={() => actions.eliminarContacto(id)}>
           Borrar contacto
         </button>
       </div>
@@ -17,4 +20,4 @@ const CardContactos = ({ contacto, borrarContacto }) => { // recibe props desest
   );
 };
 
-export default CardContactos;*/
+export default CardContactos;
